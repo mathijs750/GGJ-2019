e752e83849c35aa87f33e67ecad6d875391ad5c0
Mathijs Lemmers <mathijs750@gmail.com>
2019-01-27 05:52:38 +0100

    Update HouseController.cs

--- Assets/Scripts/Controllers/HouseController.cs
+++ Assets/Scripts/Controllers/HouseController.cs
@@ -1,3 +1,4 @@
+using Managers;
 using ScriptableObjects;
 using UnityEngine;
 using UnityEngine.Serialization;
@@ -13,11 +14,20 @@ namespace Controllers
         [SerializeField] private GameEvent _badFeedback;
         [SerializeField] private GameObject[] _blocksForQueue;
         private int _currentIndex;
+        
+        public delegate void BlockQueueEmpty();
+
+        public static event BlockQueueEmpty OnEmpty;
 
         public GameObject GetNextBlock()
         {
             _currentIndex++;
-            return _currentIndex >= _blocksForQueue.Length ? null : _blocksForQueue[_currentIndex];
+            if (_currentIndex == _blocksForQueue.Length - 1)
+            {
+                OnEmpty?.Invoke();
+                GameManager.Instance.SpawnNewHouseController();
+            }
+            return _blocksForQueue[_currentIndex];
         }
 
         private void Awake()
